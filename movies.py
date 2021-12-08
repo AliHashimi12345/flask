49c156e82344f95c8db92ac8a737e3ffb7897820
AliHashimi12345 <alihashimivns@gmail.com>
2021-12-08 19:09:57 +0530

    fifth commit

--- movies.py
+++ movies.py
@@ -6,18 +6,18 @@ class Movie(db.Model):
     __tablename__ = 'movies'  # creating a table name
     id = db.Column(db.Integer, primary_key=True)  # this is the primary key
     title = db.Column(db.String(80), nullable=False)
-    # nullable is false so the column can't be empty
+    
     year = db.Column(db.Integer, nullable=False)
     genre = db.Column(db.String(80), nullable=False)
 
     def json(self):
         return {'id': self.id, 'title': self.title,
                 'year': self.year, 'genre': self.genre}
-        # this method we are defining will convert our output to json
+        
 
     def add_movie(_title, _year, _genre):
         
-        # creating an instance of our Movie constructor
+    
         new_movie = Movie(title=_title, year=_year, genre=_genre)
         db.session.add(new_movie)  # add new movie to database session
         db.session.commit()  # commit changes to session
